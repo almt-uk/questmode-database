63ac39bf3811d5f86a6acbd0bee39eb62b345ab5
Teodor G <grigor.teodor@gmail.com>
2020-03-04 15:20:37 +0000

    Create table_answers.sql

---
+++ tables/table_answers.sql
@@ -0,0 +1,17 @@
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers`
+--
+
+DROP TABLE IF EXISTS `answers`;
+CREATE TABLE IF NOT EXISTS `answers` (
+  `answer_id` int(13) NOT NULL AUTO_INCREMENT,
+  `question_id` int(12) NOT NULL,
+  `content` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `is_right` tinyint(1) NOT NULL DEFAULT 0,
+  `order_id` int(1) NOT NULL,
+  PRIMARY KEY (`answer_id`),
+  KEY `fk_a_question` (`question_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000000000000 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
