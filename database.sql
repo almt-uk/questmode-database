1b36db7d17fde13e541a91519c1c6f0119ef8bf5
Teodor G <grigor.teodor@gmail.com>
2020-03-04 15:20:20 +0000

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 04, 2020 at 03:16 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id12807715_questmodedb`
+--
+CREATE DATABASE IF NOT EXISTS `id12807715_questmodedb` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `id12807715_questmodedb`;
