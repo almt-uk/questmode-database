c86637f7019662721a0a75b95490ecf22f1e8540
Teodor G <grigor.teodor@gmail.com>
2020-03-04 15:20:48 +0000

    Create table_questions.sql

---
+++ tables/table_questions.sql
@@ -0,0 +1,18 @@
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+CREATE TABLE IF NOT EXISTS `questions` (
+  `question_id` int(12) NOT NULL AUTO_INCREMENT,
+  `experience` int(5) NOT NULL,
+  `content` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `quizz_id` int(10) NOT NULL,
+  `duration` int(3) NOT NULL,
+  PRIMARY KEY (`question_id`),
+  KEY `fk_que_quizz` (`quizz_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000000000 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
