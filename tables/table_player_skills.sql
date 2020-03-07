7074fcea37f26314aafe261a26a713849e9f843b
Teodor G <grigor.teodor@gmail.com>
2020-03-07 19:44:00 +0000

    Create table_player_skills.sql

---
+++ tables/table_player_skills.sql
@@ -0,0 +1,15 @@
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player_skills`
+--
+
+DROP TABLE IF EXISTS `player_skills`;
+CREATE TABLE IF NOT EXISTS `player_skills` (
+  `player_id` int(6) NOT NULL,
+  `skill_id` int(6) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`player_id`,`skill_id`),
+  KEY `fk_ps_skills` (`skill_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
