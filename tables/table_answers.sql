db82a678f4050df1a089e6d9773dd22d7efce425
Teodor G <grigor.teodor@gmail.com>
2020-03-07 19:43:21 +0000

    Update table_answers.sql

--- tables/table_answers.sql
+++ tables/table_answers.sql
@@ -12,6 +12,5 @@ CREATE TABLE IF NOT EXISTS `answers` (
   `content` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `is_right` tinyint(1) NOT NULL DEFAULT 0,
   `order_id` int(1) NOT NULL,
-  PRIMARY KEY (`answer_id`),
-  KEY `fk_a_question` (`question_id`)
+  PRIMARY KEY (`answer_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000000000 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
