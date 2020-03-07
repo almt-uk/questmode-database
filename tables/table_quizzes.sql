a6823c7a18a1f3c53c9ebc87e476c9d13e4ae0ba
Teodor G <grigor.teodor@gmail.com>
2020-03-07 19:43:34 +0000

    Update table_quizzes.sql

--- tables/table_quizzes.sql
+++ tables/table_quizzes.sql
@@ -10,8 +10,9 @@ CREATE TABLE IF NOT EXISTS `quizzes` (
   `quizz_id` int(10) NOT NULL AUTO_INCREMENT,
   `creator_id` int(6) NOT NULL,
   `visibility` int(11) NOT NULL DEFAULT 0,
-  `times_played` int(5) NOT NULL DEFAULT 0,
   `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `edited_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`quizz_id`),
   KEY `fk_q_users` (`creator_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000000000 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
